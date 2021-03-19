543ff91cbdaf46fc474f35222a90d9a2924e4681
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-19 09:53:53 -0700

    created a todo class

---
+++ src/main/java/com/lambdaschool/todos/models/Todo.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.todos.models;
+
+public class Todo
+{
+    private long todoid;
+    private String description;
+    private boolean completed;
+    private User userid;
+}
